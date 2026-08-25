3026369b19cc4e4cf7e89cbe4d574e78da4452bd
Jason Rowe <jasonfrowe@gmail.com>
2026-08-24 22:45:07 -0400

    adding assets for Rankings / Congrats screen.

---
+++ images/Congrats_tiles_4bpp_palette.h
@@ -0,0 +1,26 @@
+#ifndef CONGRATS_TILES_4BPP_H
+#define CONGRATS_TILES_4BPP_H
+
+#include <stdint.h>
+
+// Palette extracted from graphics/Congrats_tiles.png
+static const uint16_t congrats_tiles_4bpp[16] = {
+    0x0000,
+    0x0020,
+    0x01A6,
+    0x036D,
+    0x05F7,
+    0x59A0,
+    0xFDAC,
+    0xF5AF,
+    0xFFFF,
+    0x0AB6,
+    0xFEF8,
+    0xEE3F,
+    0xFA28,
+    0xBAAE,
+    0x0020,
+    0x0020,
+};
+
+#endif // CONGRATS_TILES_4BPP_H
---
+++ images/Rankings_tiles_4bpp_palette.h
@@ -0,0 +1,26 @@
+#ifndef RANKINGS_TILES_4BPP_H
+#define RANKINGS_TILES_4BPP_H
+
+#include <stdint.h>
+
+// Palette extracted from graphics/Rankings_tiles.png
+static const uint16_t rankings_tiles_4bpp[16] = {
+    0x0000,
+    0x0020,
+    0x01A6,
+    0x036D,
+    0x05F7,
+    0x59A0,
+    0xFDAC,
+    0xF5AF,
+    0xFFFF,
+    0x0AB6,
+    0xFEF8,
+    0xEE3F,
+    0xFA28,
+    0xBAAE,
+    0x0020,
+    0x0020,
+};
+
+#endif // RANKINGS_TILES_4BPP_H
