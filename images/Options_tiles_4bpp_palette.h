3becf1232b212c8e6034c7c9cb14b9bd19c96849
Jason Rowe <jasonfrowe@gmail.com>
2026-08-26 12:57:44 -0400

    setting up options menu

---
+++ images/Options_tiles_4bpp_palette.h
@@ -0,0 +1,26 @@
+#ifndef OPTIONS_TILES_4BPP_H
+#define OPTIONS_TILES_4BPP_H
+
+#include <stdint.h>
+
+// Palette extracted from graphics/Options_tiles.png
+static const uint16_t options_tiles_4bpp[16] = {
+    0x0000,
+    0x0020,
+    0x01A6,
+    0x036D,
+    0x05F7,
+    0x59A0,
+    0xFDAC,
+    0xF5AF,
+    0xFFFF,
+    0x0AB6,
+    0xFEF8,
+    0xEE3F,
+    0xFA28,
+    0xBAAE,
+    0x0020,
+    0x0020,
+};
+
+#endif // OPTIONS_TILES_4BPP_H
