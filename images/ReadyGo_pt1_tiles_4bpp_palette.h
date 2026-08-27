c05d3632ef7455d0964966360a33f55dbf43c88d
Jason Rowe <jasonfrowe@gmail.com>
2026-08-27 18:30:42 -0400

    Adding countdown assets.

---
+++ images/ReadyGo_pt1_tiles_4bpp_palette.h
@@ -0,0 +1,26 @@
+#ifndef READYGO_PT1_TILES_4BPP_H
+#define READYGO_PT1_TILES_4BPP_H
+
+#include <stdint.h>
+
+// Palette extracted from graphics/ReadyGo_pt1_tiles.png
+static const uint16_t readygo_pt1_tiles_4bpp[16] = {
+    0x0000,
+    0x0A60,
+    0x402D,
+    0x036D,
+    0xF92E,
+    0xFA28,
+    0xFDAC,
+    0x8728,
+    0x21B6,
+    0x06F1,
+    0x743F,
+    0xAD75,
+    0xFCB2,
+    0x04A1,
+    0x05F7,
+    0xFFFF,
+};
+
+#endif // READYGO_PT1_TILES_4BPP_H
---
+++ images/ReadyGo_pt2_tiles_4bpp_palette.h
@@ -0,0 +1,26 @@
+#ifndef READYGO_PT2_TILES_4BPP_H
+#define READYGO_PT2_TILES_4BPP_H
+
+#include <stdint.h>
+
+// Palette extracted from graphics/ReadyGo_pt2_tiles.png
+static const uint16_t readygo_pt2_tiles_4bpp[16] = {
+    0x0000,
+    0x0A60,
+    0x402D,
+    0x036D,
+    0xF92E,
+    0xFA28,
+    0xFDAC,
+    0x8728,
+    0x21B6,
+    0x06F1,
+    0x743F,
+    0xAD75,
+    0xFCB2,
+    0x04A1,
+    0x05F7,
+    0xFFFF,
+};
+
+#endif // READYGO_PT2_TILES_4BPP_H
